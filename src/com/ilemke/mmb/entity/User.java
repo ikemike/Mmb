5a994733c2d9b282ef8129058da776a6de29cee6
ikemike <cpu.ikemike@gmail.com>
2016-04-14 20:56:06 -0500

    basic updates

---
+++ src/com/ilemke/mmb/entity/User.java
@@ -0,0 +1,26 @@
+package com.ilemke.mmb.entity;
+
+/**
+ * Based on the FBTR user.java example
+ * Represents a user of the Map_My_Brain application
+*/
+
+public class User
+{
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String password;
+
+    public User() {}
+
+    public User(int id, String firstName, String lastName, String emailAddress, String password)
+    {
+
+    }
+
+
+
+}
