e3ad1f4c32be49462ca3464a84f8958afbdae54f
ikemike <ilemke@madisoncollege.edu>
2016-05-12 16:53:27 -0500

    Start/stop recording. Hibernate fixes.

--- src/main/java/com/ilemke/mmb/controller/ViewBrain.java
+++ src/main/java/com/ilemke/mmb/controller/ViewBrain.java
@@ -44,7 +44,7 @@ public class ViewBrain extends HttpServlet  {
 
         List<Brain> brains = dao.getAllBrains();
 
-        log.debug("Sending all brain info");\
+        log.debug("Sending all brain info");
         HttpSession session = req.getSession();
         session.setAttribute("brains", brains);
         String url = "/restricted/viewBrain.jsp";
